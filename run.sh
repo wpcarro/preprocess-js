6ded44bd5d26f6daffb55d50791038caf4a94860
William Carroll <wpcarro@users.noreply.github.com>
2016-03-13 16:39:38 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@ VAL=10;
 REPLACE_REGEXP="s/${REPLACE}/${VAL}/g";
 
 # pipeline
-cat file0.js | $SED_BIN "${REPLACE_REGEXP} | $NODE_BIN;
+cat file0.js | $SED_BIN "${REPLACE_REGEXP}" | $NODE_BIN;
 
 eval unset{' NODE_BIN;',' SED_BIN;',' REPLACE;',' VAL;',' REPLACE_REGEXP;'};
